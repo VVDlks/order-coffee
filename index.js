922af26617ce58b198f4154415827ebe51f441d4
Mariya <maripolevay@gmail.com>
2026-04-21 19:17:21 +0500

    первое done

---
+++ index.js
@@ -0,0 +1,23 @@
+const newFormButton = document.querySelector('.add-button');
+if (newFormButton) {
+    newFormButton.addEventListener('click', () => {
+        const forms = document.querySelectorAll('.beverage');
+        const lastForm = forms[forms.length - 1];
+        const newForm = lastForm.cloneNode(true);
+
+        const index = forms.length +1;
+        newForm.querySelector('.beverage-count').textContent = `Напиток №${index}`;
+
+        newForm.querySelectorAll('input, select').forEach(el => {
+            el.name = `${el.name.split(' ')[0]} ${index}`
+            if (el.type === 'radio' || el.type === 'checkbox') {
+                el.checked = false;
+            }
+        });
+
+        newForm.querySelector('input[type="radio"]').checked = true;
+
+        lastForm.parentNode.insertBefore(newForm, lastForm.nextSibling);
+        console.log(lastForm);
+    })
+}
